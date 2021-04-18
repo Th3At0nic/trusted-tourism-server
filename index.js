8fd327b97e667ac89c830b4ae0bb0ce791c657af
Th3At0nic <islammdrahatul@gmail.com>
2021-04-18 23:41:52 +0600

    random commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fileupload = require("express-fileupload");
 const port = 5003;
 require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static("beauticians"));
+app.use(fileupload());
 
 const MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hz5rx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -21,9 +24,32 @@ client.connect((err) => {
 
   app.post("/addAppointment", (req, res) => {
     const appointment = req.body;
-    console.log(appointment);
     appointmentCollection.insertOne(appointment).then((result) => {
       res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/appointmentsByDate", (req, res) => {
+    const date = req.body;
+    console.log(date.date);
+    appointmentCollection
+      .find({ date: date.date })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.post("/addBeautician", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const file = req.files.file;
+    console.log(name, email, file);
+    file.mv(`${__dirname}/beauticians/${file.name}`, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send({ msg: "Failed to upload image" });
+      }
+      return res.send({ name: file.name, path: `/${file.name}` });
     });
   });
 });
